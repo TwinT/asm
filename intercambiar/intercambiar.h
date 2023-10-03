e67dd272af1bdad6b2c0971ebf46bbb21189af1c
Guada <guadarodriguezf@gmail.com>
2023-10-03 20:14:57 -0300

    fixes

---
+++ intercambiar/intercambiar.h
@@ -0,0 +1,6 @@
+#ifndef INTERCAMBIAR_H
+#define INTERCAMBIAR_H
+
+void intercambiar(char*);
+
+#endif 
--- intercambiar/main.c
+++ intercambiar/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-
+#include "intercambiar.h"
 
 int main() {
     char cadena[] = "Orga del computador";
